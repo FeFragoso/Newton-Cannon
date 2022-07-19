227d264b52ecedda7c4de563fe92c94d03fbad86
fefragoso <felipeofragoso@gmail.com>
2022-07-19 03:11:09 -0300

    Padronizado a área do universo e melhorias de responsividade

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const gravidade = 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const STAR_COUNT = 100
+let result = ""
+for(let i = 0; i < STAR_COUNT; i++){
+    result += `${randomNumber(-100, 100)}vh ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
+}
+console.log(result.substring(0, result.length - 1))
