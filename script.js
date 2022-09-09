85031b911964c0702da9212378dec6cb33e37348
fefragoso <felipeofragoso@gmail.com>
2022-09-08 23:06:47 -0300

    Agora ta indo!

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 var bola = document.getElementById('bola')
 
-var x0 = 509
-var xf
-var y0 = 382.5
-var yf
-
-var gravidade = 5
 var velocidade = 1
+var gravidade = 0.5
+
+var x = 509
+var y = 382.5
 
 function movimento() {
-    if (x0>10 && x0<991 && y0>10 && y0<591) {
-        tempoX = Math.sqrt((2*y0)/gravidade)
-        tempoY = Math.sqrt((2*x0)/gravidade)
+    if (x>10 && x<991 && y>10 && y<591) {
 
-        xf = x0+(velocidade*tempoX)
-        yf = y0-(velocidade*tempoY)
+        if (x>500.5 && y>=300.5) {
+            x += velocidade
+            y += (gravidade*-1)
+        }
+        else if (x>=500.5 && y<300.5) {
+            x += (velocidade*-1)
+            y += (gravidade*-1)
+        }
+        else if (x<500.5 && y<300.5) {
+            x += (velocidade*-1)
+            y += gravidade
+        }
+        else {
+            x += velocidade
+            y += gravidade
+        }
 
-        bola.style.left = xf + 'px'
-        bola.style.bottom = yf + 'px'
-
-        x0 = xf
-        y0 = yf
-
-        console.log(x0, y0)
+        bola.style.left = x + 'px'
+        bola.style.bottom = y + 'px'
         
         loop = requestAnimationFrame(movimento)
     }
 }
 
 function load() {
-    x0 = 509
-    y0 = 382.5
-
     movimento()
 }
 
 window.addEventListener("click", load)
-
-
-// 1Q { x = 500.5 - 1001 | y = 300.5 - 601 }
-// 2Q { x = 500.5 - 1001 | y = 0 - 300.5 }
