420a018588559374d9519023cca6921176ccfc48
fefragoso <felipeofragoso@gmail.com>
2022-09-09 16:53:33 -0300

    Aceleração faltando!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var bola = document.getElementById('bola')
-var velocidade = 3
+var velocidade = 2.5
 var gravidade = 1
 var loop
 var x = 509
@@ -9,7 +9,6 @@ function movimento() {
     cancelAnimationFrame(loop)
 
     if (x>10 && x<991 && y>10 && y<591) {
-        
         if (x>500.5 && y>=300.5) {
             x += velocidade
             y += (gravidade*-1)
@@ -29,13 +28,13 @@ function movimento() {
         
         bola.style.left = x + 'px'
         bola.style.bottom = y + 'px'
-
-        loop = requestAnimationFrame(movimento) 
     }
 }
 
 function reset() {
-    cancelAnimationFrame(loop)
+    clearInterval(loop)
+
+    aceleracao = 0
 
     x = 509
     y = 383
@@ -43,3 +42,16 @@ function reset() {
     bola.style.left = '509px'
     bola.style.bottom = '383px'
 }
+
+function disparar() {
+    loop = setInterval(movimento, 50)
+}
+
+// ------------------------------------------------
+
+// PONTO DE ORIGEM { X = 501 | Y = 301 }
+
+e = 360
+t = 
+angulo = 
+r =  // x = 509 | y = 383
