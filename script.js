b3da8dc3d92f32c196f31e59d55e72c3d0581713
fefragoso <felipeofragoso@gmail.com>
2022-09-12 02:37:12 -0300

    Reformulação total da lógica do projeto

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 var bola = document.getElementById('bola')
-var velocidade = 2.5
-var gravidade = 1
+var angulo = 84
+var radiano
+var raio
 var loop
 var x = 509
 var y = 383
 
-function movimento() {
+function calculo() {
+    if (x>10 && x<991 && y>10 && y<591) {
+        x -= 501
+        y -= 301
+
+        raio = Math.sqrt((x**2)+(y**2))
+
+        radiano = (angulo*Math.PI)/180
+
+        x = 501+(Math.cos(radiano)*raio)
+        y = 301+(Math.sin(radiano)*raio)
+    }
+
+    angulo--
+
+    return x, y
+}
+
+function disparo() {
     cancelAnimationFrame(loop)
 
-    if (x>10 && x<991 && y>10 && y<591) {
-        if (x>500.5 && y>=300.5) {
-            x += velocidade
-            y += (gravidade*-1)
-        }
-        else if (x>=500.5 && y<300.5) {
-            x += (velocidade*-1)
-            y += (gravidade*-1)
-        }
-        else if (x<500.5 && y<300.5) {
-            x += (velocidade*-1)
-            y += gravidade
-        }
-        else {
-            x += velocidade
-            y += gravidade
-        }
-        
-        bola.style.left = x + 'px'
-        bola.style.bottom = y + 'px'
-    }
+    x, y = calculo()
+
+    bola.style.left = x + 'px'
+    bola.style.bottom = y + 'px'
+
+    loop = requestAnimationFrame(disparo)
 }
 
 function reset() {
-    clearInterval(loop)
+    cancelAnimationFrame(loop)
 
-    aceleracao = 0
+    angulo = 84
 
     x = 509
     y = 383
@@ -42,16 +46,3 @@ function reset() {
     bola.style.left = '509px'
     bola.style.bottom = '383px'
 }
-
-function disparar() {
-    loop = setInterval(movimento, 50)
-}
-
-// ------------------------------------------------
-
-// PONTO DE ORIGEM { X = 501 | Y = 301 }
-
-e = 360
-t = 
-angulo = 
-r =  // x = 509 | y = 383
